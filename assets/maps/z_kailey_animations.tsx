dc1504fd035448902c3cf957a92f51eeda27bc8b
voidcultist <mirameliora@protonmail.com>
2026-03-27 19:24:37 +0100

    map updates

---
+++ assets/maps/z_kailey_animations.tsx
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="2026.02.12" name="kailey_animations" tilewidth="16" tileheight="16" tilecount="7770" columns="42">
+ <image source="../../../../../../../../../../Users/User/Documents/SDVModding/Content (unpacked)/Maps/Custom tilesheets/[CP] Kailey's Tilesheets/assets/Outdoors/BlueGrass/kailey_animations.png" width="672" height="2960"/>
+ <tile id="2142">
+  <animation>
+   <frame tileid="2142" duration="200"/>
+   <frame tileid="2143" duration="200"/>
+   <frame tileid="2144" duration="200"/>
+   <frame tileid="2145" duration="200"/>
+   <frame tileid="2146" duration="200"/>
+   <frame tileid="2147" duration="200"/>
+   <frame tileid="2148" duration="200"/>
+   <frame tileid="2149" duration="200"/>
+   <frame tileid="2150" duration="200"/>
+   <frame tileid="2151" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="2226">
+  <animation>
+   <frame tileid="2226" duration="200"/>
+   <frame tileid="2227" duration="200"/>
+   <frame tileid="2228" duration="200"/>
+   <frame tileid="2229" duration="200"/>
+   <frame tileid="2230" duration="200"/>
+   <frame tileid="2231" duration="200"/>
+   <frame tileid="2232" duration="200"/>
+   <frame tileid="2233" duration="200"/>
+   <frame tileid="2234" duration="200"/>
+   <frame tileid="2235" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
